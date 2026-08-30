7ffd104c242f20702359427dd9b95e74f86e9c62
victor Tshimbadi Mputu <victor.mpututshimbadi@vodacom.cd>
2026-08-30 14:16:13 +0100

    feat: use * in CORS during development mode

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,9 @@ import routes from './routes';
 const app: Application = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: process.env.NODE_ENV === 'development' ? '*' : process.env.ALLOWED_ORIGINS?.split(',') || false,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
